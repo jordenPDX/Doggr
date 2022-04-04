d4395c73b2bfcdc525e682aad21810380a8e68fe
Casey Bailey <soniczentropy@gmail.com>
2022-04-04 12:31:06 -0500

    Added eslint and packages supporting it

--- nexpress/server.js
+++ nexpress/server.js
@@ -2,11 +2,11 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer( (req, res) => {
-	res.writeHead(200, { "Content-Type": "text/html"});	
-	res.write("Hello World\n");
-	res.end();
-})
+  res.writeHead(200, { "Content-Type": "text/html"});
+  res.write("Hello World\n");
+  res.end();
+});
 
 server.listen(9000, () => {
-	console.log("Server running on port 9000");
-})
+  console.log("Server running on port 9000");
+});
